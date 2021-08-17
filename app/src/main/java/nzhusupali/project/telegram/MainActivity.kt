856fc3324510083a0807fb44183ca07fc1c24311
nzhusupali <Razor1000011@gmail.com>
2021-08-17 19:21:07 +0300

    1. Подготовка проекта

--- app/src/main/java/nzhusupali/project/telegram/MainActivity.kt
+++ app/src/main/java/nzhusupali/project/telegram/MainActivity.kt
@@ -2,10 +2,18 @@ package nzhusupali.project.telegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nzhusupali.project.telegram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
