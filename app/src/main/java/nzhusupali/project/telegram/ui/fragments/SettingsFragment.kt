a47c2d1ded1e77f3e10e085ce6f08fcee1c21ea2
nzhusupali <Razor1000011@gmail.com>
2021-08-20 16:44:30 +0300

    7. Добавил Options menu с иконками.

--- app/src/main/java/nzhusupali/project/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/nzhusupali/project/telegram/ui/fragments/SettingsFragment.kt
@@ -1,9 +1,17 @@
 package nzhusupali.project.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import nzhusupali.project.telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+
     }
 }
